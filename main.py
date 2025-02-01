1a771b1bd140a758ab624136292e0d2b909c72ea
ItsAndrewC! <andew73885@gmail.com>
2025-02-01 11:12:37 -0500

    server successfully server index.html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<p>Main Page</p>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
