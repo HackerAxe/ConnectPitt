b287f076c8e243013d6a78cc2595a750dbb4297b
trackrunnner8935 <113869162+trackrunnner8935@users.noreply.github.com>
2025-02-01 14:36:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from user_model import Base, User
 
 app = Flask(__name__)
 
@@ -8,7 +9,33 @@ def main():
 
 @app.route("/results")
 def results():
-    return render_template('results.html')
+    name=request.form["name"]
+    phoneNumber=request.form["phoneNumber"]
+    LinkedIn=request.form["LinkedIn"]
+    question_one=request.form["question_one"]
+    question_two=request.form["question_two"]
+    question_three=request.form["question_three"]
+    question_four=request.form["question_four"]
+    question_five=request.form["question_five"]
+    question_six=request.form["question_six"]
+    question_seven=request.form["question_seven"]
+    question_eight=request.form["question_eight"]
+
+    new_user=User(name=name, phoneNumber=phoneNumber, LinkedIn=LinkedIn, question_one=question_one,
+                  question_two=question_two, question_three=question_three, question_four=question_four,
+                  question_five=question_five, question_six=question_six, question_seven=question_seven,
+                  question_eight=question_eight)
+
+    new_user_in_JSON_format=new_user.toJSON()
+    print(new_order_in_JSON_format)
+
+    try:
+        session.add(new_user)
+        session.commit()
+        return render_template("results.html", user=new_user_in_JSON_format)
+    except:
+        return 'Issue adding task'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
