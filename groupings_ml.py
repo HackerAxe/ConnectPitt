c4efcae0e942b7aafb0daaae808c6581e89db105
ItsAndrewC! <andew73885@gmail.com>
2025-02-02 01:59:14 -0500

    adding ML script

--- groupings_ml.py
+++ groupings_ml.py
@@ -1,3 +1,3 @@
 # Questions -> [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8]
 # Labels -> [Sensing, Thinking, Rational, Logical, Assertive, Passive, Futuristic, Historian]
-# Implemented using Nearest Neighbors 
+# Implemented using K Nearest Neighbors with Dimensionality Reduction (PCA)
